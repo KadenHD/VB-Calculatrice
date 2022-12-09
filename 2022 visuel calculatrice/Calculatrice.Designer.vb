3de74840c1ee6f12921f5dec45d39be972ec95dd
KadenHD <pierraored@gmail.com>
2022-12-09 19:42:19 +0100

    add casio front

--- 2022 visuel calculatrice/Calculatrice.Designer.vb
+++ 2022 visuel calculatrice/Calculatrice.Designer.vb
@@ -46,6 +46,9 @@ Partial Class Form_Calculatrice
         Me.BT_Ajouter = New System.Windows.Forms.Button()
         Me.BT_Changer_Signe = New System.Windows.Forms.Button()
         Me.TB_Log = New System.Windows.Forms.TextBox()
+        Me.L_Casio = New System.Windows.Forms.Label()
+        Me.PB_Bordure_Affichage = New System.Windows.Forms.PictureBox()
+        CType(Me.PB_Bordure_Affichage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TB_Affichage
@@ -63,218 +66,239 @@ Partial Class Form_Calculatrice
         '
         'BT_0
         '
+        Me.BT_0.BackColor = System.Drawing.Color.Silver
         Me.BT_0.Location = New System.Drawing.Point(53, 192)
         Me.BT_0.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_0.Name = "BT_0"
         Me.BT_0.Size = New System.Drawing.Size(35, 30)
         Me.BT_0.TabIndex = 0
         Me.BT_0.Text = "0"
-        Me.BT_0.UseVisualStyleBackColor = True
+        Me.BT_0.UseVisualStyleBackColor = False
         '
         'BT_Virgule
         '
+        Me.BT_Virgule.BackColor = System.Drawing.Color.Silver
         Me.BT_Virgule.Location = New System.Drawing.Point(93, 192)
         Me.BT_Virgule.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Virgule.Name = "BT_Virgule"
         Me.BT_Virgule.Size = New System.Drawing.Size(35, 30)
         Me.BT_Virgule.TabIndex = 10
         Me.BT_Virgule.Text = ","
-        Me.BT_Virgule.UseVisualStyleBackColor = True
+        Me.BT_Virgule.UseVisualStyleBackColor = False
         '
         'BT_Egal
         '
+        Me.BT_Egal.BackColor = System.Drawing.Color.Silver
         Me.BT_Egal.Location = New System.Drawing.Point(133, 192)
         Me.BT_Egal.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Egal.Name = "BT_Egal"
         Me.BT_Egal.Size = New System.Drawing.Size(35, 30)
         Me.BT_Egal.TabIndex = 19
         Me.BT_Egal.Text = "="
-        Me.BT_Egal.UseVisualStyleBackColor = True
+        Me.BT_Egal.UseVisualStyleBackColor = False
         '
         'BT_1
         '
+        Me.BT_1.BackColor = System.Drawing.Color.Silver
         Me.BT_1.Location = New System.Drawing.Point(12, 157)
         Me.BT_1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_1.Name = "BT_1"
         Me.BT_1.Size = New System.Drawing.Size(35, 30)
         Me.BT_1.TabIndex = 1
         Me.BT_1.Text = "1"
-        Me.BT_1.UseVisualStyleBackColor = True
+        Me.BT_1.UseVisualStyleBackColor = False
         '
         'BT_2
         '
+        Me.BT_2.BackColor = System.Drawing.Color.Silver
         Me.BT_2.Location = New System.Drawing.Point(53, 157)
         Me.BT_2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_2.Name = "BT_2"
         Me.BT_2.Size = New System.Drawing.Size(35, 30)
         Me.BT_2.TabIndex = 2
         Me.BT_2.Text = "2"
-        Me.BT_2.UseVisualStyleBackColor = True
+        Me.BT_2.UseVisualStyleBackColor = False
         '
         'BT_3
         '
+        Me.BT_3.BackColor = System.Drawing.Color.Silver
         Me.BT_3.Location = New System.Drawing.Point(93, 157)
         Me.BT_3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_3.Name = "BT_3"
         Me.BT_3.Size = New System.Drawing.Size(35, 30)
         Me.BT_3.TabIndex = 3
         Me.BT_3.Text = "3"
-        Me.BT_3.UseVisualStyleBackColor = True
+        Me.BT_3.UseVisualStyleBackColor = False
         '
         'BT_Soustraire
         '
+        Me.BT_Soustraire.BackColor = System.Drawing.Color.Silver
         Me.BT_Soustraire.Location = New System.Drawing.Point(134, 123)
         Me.BT_Soustraire.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Soustraire.Name = "BT_Soustraire"
         Me.BT_Soustraire.Size = New System.Drawing.Size(35, 30)
         Me.BT_Soustraire.TabIndex = 17
         Me.BT_Soustraire.Text = "-"
-        Me.BT_Soustraire.UseVisualStyleBackColor = True
+        Me.BT_Soustraire.UseVisualStyleBackColor = False
         '
         'BT_4
         '
+        Me.BT_4.BackColor = System.Drawing.Color.Silver
         Me.BT_4.Location = New System.Drawing.Point(12, 123)
         Me.BT_4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_4.Name = "BT_4"
         Me.BT_4.Size = New System.Drawing.Size(35, 30)
         Me.BT_4.TabIndex = 4
         Me.BT_4.Text = "4"
-        Me.BT_4.UseVisualStyleBackColor = True
+        Me.BT_4.UseVisualStyleBackColor = False
         '
         'BT_5
         '
+        Me.BT_5.BackColor = System.Drawing.Color.Silver
         Me.BT_5.Location = New System.Drawing.Point(53, 123)
         Me.BT_5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_5.Name = "BT_5"
         Me.BT_5.Size = New System.Drawing.Size(35, 30)
         Me.BT_5.TabIndex = 5
         Me.BT_5.Text = "5"
-        Me.BT_5.UseVisualStyleBackColor = True
+        Me.BT_5.UseVisualStyleBackColor = False
         '
         'BT_6
         '
+        Me.BT_6.BackColor = System.Drawing.Color.Silver
         Me.BT_6.Location = New System.Drawing.Point(93, 123)
         Me.BT_6.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_6.Name = "BT_6"
         Me.BT_6.Size = New System.Drawing.Size(35, 30)
         Me.BT_6.TabIndex = 6
         Me.BT_6.Text = "6"
-        Me.BT_6.UseVisualStyleBackColor = True
+        Me.BT_6.UseVisualStyleBackColor = False
         '
         'BT_Multiplier
         '
+        Me.BT_Multiplier.BackColor = System.Drawing.Color.Silver
         Me.BT_Multiplier.Location = New System.Drawing.Point(134, 88)
         Me.BT_Multiplier.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Multiplier.Name = "BT_Multiplier"
         Me.BT_Multiplier.Size = New System.Drawing.Size(35, 30)
         Me.BT_Multiplier.TabIndex = 16
         Me.BT_Multiplier.Text = "X"
-        Me.BT_Multiplier.UseVisualStyleBackColor = True
+        Me.BT_Multiplier.UseVisualStyleBackColor = False
         '
         'BT_7
         '
+        Me.BT_7.BackColor = System.Drawing.Color.Silver
         Me.BT_7.Location = New System.Drawing.Point(12, 88)
         Me.BT_7.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_7.Name = "BT_7"
         Me.BT_7.Size = New System.Drawing.Size(35, 30)
         Me.BT_7.TabIndex = 7
         Me.BT_7.Text = "7"
-        Me.BT_7.UseVisualStyleBackColor = True
+        Me.BT_7.UseVisualStyleBackColor = False
         '
         'BT_8
         '
+        Me.BT_8.BackColor = System.Drawing.Color.Silver
         Me.BT_8.Location = New System.Drawing.Point(53, 88)
         Me.BT_8.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_8.Name = "BT_8"
         Me.BT_8.Size = New System.Drawing.Size(35, 30)
         Me.BT_8.TabIndex = 8
         Me.BT_8.Text = "8"
-        Me.BT_8.UseVisualStyleBackColor = True
+        Me.BT_8.UseVisualStyleBackColor = False
         '
         'BT_9
         '
+        Me.BT_9.BackColor = System.Drawing.Color.Silver
         Me.BT_9.Location = New System.Drawing.Point(94, 88)
         Me.BT_9.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_9.Name = "BT_9"
         Me.BT_9.Size = New System.Drawing.Size(35, 30)
         Me.BT_9.TabIndex = 9
         Me.BT_9.Text = "9"
-        Me.BT_9.UseVisualStyleBackColor = True
+        Me.BT_9.UseVisualStyleBackColor = False
         '
         'BT_Diviser
         '
-        Me.BT_Diviser.Location = New System.Drawing.Point(93, 54)
+        Me.BT_Diviser.BackColor = System.Drawing.Color.Silver
+        Me.BT_Diviser.Location = New System.Drawing.Point(135, 54)
         Me.BT_Diviser.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Diviser.Name = "BT_Diviser"
         Me.BT_Diviser.Size = New System.Drawing.Size(35, 30)
         Me.BT_Diviser.TabIndex = 15
         Me.BT_Diviser.Text = "/"
-        Me.BT_Diviser.UseVisualStyleBackColor = True
+        Me.BT_Diviser.UseVisualStyleBackColor = False
         '
         'BT_Supprimer
         '
-        Me.BT_Supprimer.Image = Global._2022_visuel_calculatrice.My.Resources.Resources.backspace
+        Me.BT_Supprimer.BackColor = System.Drawing.Color.Silver
         Me.BT_Supprimer.Location = New System.Drawing.Point(12, 192)
         Me.BT_Supprimer.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Supprimer.Name = "BT_Supprimer"
         Me.BT_Supprimer.Size = New System.Drawing.Size(35, 30)
         Me.BT_Supprimer.TabIndex = 11
-        Me.BT_Supprimer.UseVisualStyleBackColor = True
+        Me.BT_Supprimer.Text = "DEL"
+        Me.BT_Supprimer.UseVisualStyleBackColor = False
         '
         'BT_C
         '
+        Me.BT_C.BackColor = System.Drawing.Color.Silver
         Me.BT_C.Location = New System.Drawing.Point(12, 54)
         Me.BT_C.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_C.Name = "BT_C"
         Me.BT_C.Size = New System.Drawing.Size(35, 30)
         Me.BT_C.TabIndex = 12
         Me.BT_C.Text = "C"
-        Me.BT_C.UseVisualStyleBackColor = True
+        Me.BT_C.UseVisualStyleBackColor = False
         '
         'BT_CE
         '
+        Me.BT_CE.BackColor = System.Drawing.Color.Silver
         Me.BT_CE.Location = New System.Drawing.Point(53, 54)
         Me.BT_CE.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_CE.Name = "BT_CE"
         Me.BT_CE.Size = New System.Drawing.Size(35, 30)
         Me.BT_CE.TabIndex = 13
         Me.BT_CE.Text = "CE"
-        Me.BT_CE.UseVisualStyleBackColor = True
+        Me.BT_CE.UseVisualStyleBackColor = False
         '
         'BT_Quitter
         '
-        Me.BT_Quitter.Image = Global._2022_visuel_calculatrice.My.Resources.Resources.off
-        Me.BT_Quitter.Location = New System.Drawing.Point(134, 54)
+        Me.BT_Quitter.BackColor = System.Drawing.Color.Silver
+        Me.BT_Quitter.Location = New System.Drawing.Point(94, 54)
         Me.BT_Quitter.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Quitter.Name = "BT_Quitter"
         Me.BT_Quitter.Size = New System.Drawing.Size(35, 15)
         Me.BT_Quitter.TabIndex = 20
-        Me.BT_Quitter.UseVisualStyleBackColor = True
+        Me.BT_Quitter.Text = "OFF"
+        Me.BT_Quitter.UseVisualStyleBackColor = False
         '
         'BT_Ajouter
         '
+        Me.BT_Ajouter.BackColor = System.Drawing.Color.Silver
         Me.BT_Ajouter.Location = New System.Drawing.Point(134, 157)
         Me.BT_Ajouter.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Ajouter.Name = "BT_Ajouter"
         Me.BT_Ajouter.Size = New System.Drawing.Size(35, 30)
         Me.BT_Ajouter.TabIndex = 18
         Me.BT_Ajouter.Text = "+"
-        Me.BT_Ajouter.UseVisualStyleBackColor = True
+        Me.BT_Ajouter.UseVisualStyleBackColor = False
         '
         'BT_Changer_Signe
         '
-        Me.BT_Changer_Signe.Location = New System.Drawing.Point(134, 69)
+        Me.BT_Changer_Signe.BackColor = System.Drawing.Color.Silver
+        Me.BT_Changer_Signe.Location = New System.Drawing.Point(94, 69)
         Me.BT_Changer_Signe.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.BT_Changer_Signe.Name = "BT_Changer_Signe"
         Me.BT_Changer_Signe.Size = New System.Drawing.Size(35, 15)
         Me.BT_Changer_Signe.TabIndex = 14
         Me.BT_Changer_Signe.Text = "+/-"
-        Me.BT_Changer_Signe.UseVisualStyleBackColor = True
+        Me.BT_Changer_Signe.UseVisualStyleBackColor = False
         '
         'TB_Log
         '
         Me.TB_Log.BackColor = System.Drawing.Color.Silver
-        Me.TB_Log.Location = New System.Drawing.Point(13, 11)
+        Me.TB_Log.Location = New System.Drawing.Point(13, 13)
         Me.TB_Log.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TB_Log.Name = "TB_Log"
         Me.TB_Log.ReadOnly = True
@@ -283,12 +307,32 @@ Partial Class Form_Calculatrice
         Me.TB_Log.TabIndex = 9999
         Me.TB_Log.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
+        'L_Casio
+        '
+        Me.L_Casio.AutoSize = True
+        Me.L_Casio.BackColor = System.Drawing.Color.Transparent
+        Me.L_Casio.Location = New System.Drawing.Point(53, 0)
+        Me.L_Casio.Name = "L_Casio"
+        Me.L_Casio.Size = New System.Drawing.Size(111, 15)
+        Me.L_Casio.TabIndex = 10000
+        Me.L_Casio.Text = "Casio Premium 87+"
+        '
+        'PB_Bordure_Affichage
+        '
+        Me.PB_Bordure_Affichage.BackColor = System.Drawing.Color.Black
+        Me.PB_Bordure_Affichage.Location = New System.Drawing.Point(11, 11)
+        Me.PB_Bordure_Affichage.Name = "PB_Bordure_Affichage"
+        Me.PB_Bordure_Affichage.Size = New System.Drawing.Size(157, 34)
+        Me.PB_Bordure_Affichage.TabIndex = 10001
+        Me.PB_Bordure_Affichage.TabStop = False
+        '
         'Form_Calculatrice
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(181, 234)
+        Me.Controls.Add(Me.L_Casio)
         Me.Controls.Add(Me.TB_Log)
         Me.Controls.Add(Me.BT_Changer_Signe)
         Me.Controls.Add(Me.BT_Ajouter)
@@ -312,14 +356,15 @@ Partial Class Form_Calculatrice
         Me.Controls.Add(Me.BT_Virgule)
         Me.Controls.Add(Me.BT_0)
         Me.Controls.Add(Me.TB_Affichage)
+        Me.Controls.Add(Me.PB_Bordure_Affichage)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.KeyPreview = True
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.MaximizeBox = False
         Me.Name = "Form_Calculatrice"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Calculatrice"
+        CType(Me.PB_Bordure_Affichage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -348,4 +393,6 @@ Partial Class Form_Calculatrice
     Friend WithEvents BT_Ajouter As Button
     Friend WithEvents BT_Changer_Signe As Button
     Friend WithEvents TB_Log As TextBox
+    Friend WithEvents L_Casio As Label
+    Friend WithEvents PB_Bordure_Affichage As PictureBox
 End Class
--- 2022 visuel calculatrice/Calculatrice.vb
+++ 2022 visuel calculatrice/Calculatrice.vb
@@ -4,37 +4,34 @@
 '| Date : 09/12/2022                      |
 '|                                        |
 '| Utilite :                              |
-'| Le Programme réaliser des opérations   |
+'| Le Programme réalise des opérations    |
 '| nombres négatifs, opérations multiples |
 '| et priorités de signes compris.        |
 ' ----------------------------------------
 
 ' Libs
 Imports System.Drawing.Text
+Imports System.Globalization
 Imports System.IO
 
 Public Class Form_Calculatrice
+    ' Le Form de la calculatrice '
 
     ' Vars
     Dim nouveauResultat As Boolean = False
     Dim nouveauNombre As Boolean = False
-    Dim equation As String = ""
 
     ' Consts
     ReadOnly messageErreur As String = "ERREUR SUR OPERATION"
 
     Private Sub Form_Calculatrice_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Au lancement du form '
+
         ' Attribution du style à tout le Form '
-        Dim pf = New PrivateFontCollection()
-        pf.AddFontFile(Path.GetFullPath(Application.StartupPath & "\..\..\..\Resources\Calculator.ttf"))
-        Me.Font = New Font(pf.Families(0), 34, FontStyle.Bold)
-        Me.ForeColor = Color.Blue
-        TB_Affichage.ForeColor = Color.Blue
-        TB_Log.ForeColor = Color.DarkBlue
+        Visuel_Mode_Casio()
         ' Instancier les variables '
         TB_Log.ResetText()
     End Sub
-
 
     Private Sub BT_Operateurs_Click(sender As Object, e As EventArgs) Handles BT_Ajouter.Click, BT_Soustraire.Click, BT_Multiplier.Click, BT_Diviser.Click
         ' Gestion de l'attribution des opérateurs '
@@ -52,14 +49,14 @@ Public Class Form_Calculatrice
 
         Dim res() As Char = TB_Log.Text
         If res <> "" Then
-            If nouveauNombre = True And (res(res.Length - 1) = "+" Or res(res.Length - 1) = "-" Or res(res.Length - 1) = "X" Or res(res.Length - 1) = "/") Then
+            If nouveauNombre = True And (res(res.Length - 1) = "+" Or res(res.Length - 1) = "-" Or res(res.Length - 1) = "X" Or res(res.Length - 1) = "/") Then ' Changement d'opérateur '
                 res(res.Length - 1) = sender.text
                 TB_Log.Text = res
-            Else
+            Else ' Ajout standard '
                 TB_Log.Text &= TB_Affichage.Text & sender.text
                 nouveauNombre = True
             End If
-        Else
+        Else ' Ajout standard '
             TB_Log.Text &= TB_Affichage.Text & sender.text
             nouveauNombre = True
         End If
@@ -76,10 +73,22 @@ Public Class Form_Calculatrice
 
         Dim res() As Char = TB_Affichage.Text
 
-        If res.Length = 1 And res(0) = "0" Then
-            res(0) = sender.text
-            TB_Affichage.Text = res
-        Else
+        If res.Length = 1 Then ' Remplace 0 par le chiffre '
+            If res(0) = "0" Then
+                res(0) = sender.text
+                TB_Affichage.Text = res
+            Else
+                TB_Affichage.Text &= sender.text
+            End If
+
+        ElseIf res.Length = 2 Then ' Remplace -0 par le chiffre negatif '
+            If res(0) = "-" And res(1) = "0" Then
+                res(1) = sender.text
+                TB_Affichage.Text = res
+            Else
+                TB_Affichage.Text &= sender.text
+            End If
+        Else ' Sinon concaténation '
             TB_Affichage.Text &= sender.text
         End If
     End Sub
@@ -91,7 +100,7 @@ Public Class Form_Calculatrice
 
         Changement_Nombre_Ou_Resultat()
 
-        ' S'il y a déjà une virgule, quitter le programme '
+        ' S'il y a déjà une virgule, quitter la méthode '
         Dim res() As Char = TB_Affichage.Text
         For i = 0 To res.Length - 1
             If res(i) = "," Then
@@ -99,11 +108,13 @@ Public Class Form_Calculatrice
             End If
         Next
 
+        ' S'il y a aucune virgule '
         TB_Affichage.Text &= sender.text
     End Sub
 
     Private Sub Changement_Nombre_Ou_Resultat()
         ' Prend en main le reset pour la méthode de click des chiffres et virgule '
+
         If nouveauNombre = True Or nouveauResultat = True Then
             If nouveauNombre = True Then
                 nouveauNombre = False
@@ -121,16 +132,17 @@ Public Class Form_Calculatrice
 
         Supprimer_Message_Erreur()
 
-        If nouveauResultat = True Or nouveauNombre = True Then
+        If nouveauResultat = True Or nouveauNombre = True Then ' En cas de resultat reset les logs, si nombre, rien '
             If nouveauResultat = True Then
                 TB_Log.ResetText()
-                equation = ""
             End If
-        ElseIf TB_Affichage.Text.Length = 1 Then
+        ElseIf TB_Affichage.Text.Length = 1 Then ' Si de taille 1 remplace par 0 '
             TB_Affichage.Text = "0"
-        ElseIf TB_Affichage.Text <> "0" Then
+        ElseIf TB_Affichage.Text.Length = 2 And TB_Affichage.Text(0) = "-" Then ' Si négatif de taille 1 remplace par -0 '
+            TB_Affichage.Text = "-0"
+        ElseIf TB_Affichage.Text <> "0" Then ' Si différent de 0, supprime le dernier caractère '
             TB_Affichage.Text = TB_Affichage.Text.Remove(TB_Affichage.Text.Length - 1)
-        Else
+        Else ' Sinon remplace par 0 '
             TB_Affichage.Text = "0"
         End If
     End Sub
@@ -143,14 +155,15 @@ Public Class Form_Calculatrice
         TB_Affichage.Text = Format_Affichage_Nombre(TB_Affichage.Text)
 
         Dim equation_str As String = TB_Log.Text & TB_Affichage.Text
-        equation_str = equation_str.Replace(",", ".").Replace("X", "*")
+        equation_str = equation_str.Replace(",", ".").Replace("X", "*") ' Rend la string exploitable mathématiquement '
 
+        ' Réalise le calcul et retourne une erreur en cas d'exception '
         If nouveauResultat = False Then
             Try
                 Dim calcul = New DataTable().Compute(equation_str, Nothing)
                 calcul = Math.Round(calcul, 2)
-                TB_Log.Text = equation_str.Replace(".", ",").Replace("*", "X") & sender.text
-                Dim result As Decimal = calcul ' Si pas décimal, Throw l'erreur '
+                TB_Log.Text = equation_str.Replace(".", ",").Replace("*", "X") & sender.text ' Rend la formule visuellement adaptée '
+                Dim result As Double = calcul ' Si pas décimal, Throw l'erreur '
                 TB_Affichage.Text = result
                 nouveauResultat = True
             Catch ex As Exception
@@ -165,7 +178,6 @@ Public Class Form_Calculatrice
         If TB_Affichage.Text = messageErreur Then
             TB_Log.ResetText()
             TB_Affichage.Text = "0"
-            equation = ""
             nouveauNombre = False
             nouveauResultat = False
         End If
@@ -185,7 +197,6 @@ Public Class Form_Calculatrice
 
         TB_Log.ResetText()
         TB_Affichage.Text = "0"
-        equation = ""
         nouveauNombre = False
         nouveauResultat = False
     End Sub
@@ -197,7 +208,6 @@ Public Class Form_Calculatrice
 
         If nouveauResultat = True Then
             TB_Log.ResetText()
-            equation = ""
         End If
 
         TB_Affichage.Text = "0"
@@ -219,11 +229,13 @@ Public Class Form_Calculatrice
 
     Private Shared Function Est_Negatif(str() As Char)
         ' Permet de savoir si un nombre est négatif '
+
         If str.Length <> 0 Then
             If str(0) = "-" Then
                 Return True
             End If
         End If
+
         Return False
     End Function
 
@@ -242,4 +254,130 @@ Public Class Form_Calculatrice
 
         Return text
     End Function
+
+    Public Shared Function HexToColor(ByVal hexColor As String) As Color
+        ' Prend une string de couleur hexadecimal et la retourne en obj Color '
+
+        If hexColor.IndexOf("#"c) <> -1 Then
+            hexColor = hexColor.Replace("#", "")
+        End If
+
+        Dim red As Integer = 0
+        Dim green As Integer = 0
+        Dim blue As Integer = 0
+
+        If hexColor.Length = 6 Then
+            red = Integer.Parse(hexColor.Substring(0, 2), NumberStyles.AllowHexSpecifier)
+            green = Integer.Parse(hexColor.Substring(2, 2), NumberStyles.AllowHexSpecifier)
+            blue = Integer.Parse(hexColor.Substring(4, 2), NumberStyles.AllowHexSpecifier)
+        ElseIf hexColor.Length = 3 Then
+            red = Integer.Parse(hexColor(0).ToString() + hexColor(0).ToString(), NumberStyles.AllowHexSpecifier)
+            green = Integer.Parse(hexColor(1).ToString() + hexColor(1).ToString(), NumberStyles.AllowHexSpecifier)
+            blue = Integer.Parse(hexColor(2).ToString() + hexColor(2).ToString(), NumberStyles.AllowHexSpecifier)
+        End If
+
+        Return Color.FromArgb(red, green, blue)
+    End Function
+
+    Private Sub Visuel_Mode_Casio()
+        ' Transforme visuellement en Casio '
+
+        ' Attribution des couleurs '
+        Dim couleur_blanc As Color = HexToColor("#ffffff")
+        Dim couleur_noir As Color = HexToColor("#000000")
+        Dim couleur_vert_fonce As Color = HexToColor("#3c5454")
+        Dim couleur_vert As Color = HexToColor("#6d9d7f")
+        Dim couleur_vert_clair As Color = HexToColor("#c0e9d7")
+
+        ' Attribution des Polices '
+        Dim pf = New PrivateFontCollection()
+        pf.AddFontFile(Path.GetFullPath(Application.StartupPath & "\..\..\..\Resources\Calculator.ttf"))
+        Me.Font = New Font(pf.Families(0), 34, FontStyle.Bold)
+        BT_Changer_Signe.Font = New Font(pf.Families(0), 22, FontStyle.Bold)
+        BT_Quitter.Font = New Font(pf.Families(0), 22, FontStyle.Bold)
+        BT_Supprimer.Font = New Font(pf.Families(0), 26, FontStyle.Bold)
+        L_Casio.Font = New Font(pf.Families(0), 24, FontStyle.Bold)
+
+        ' Visuel Général '
+        Me.BackColor = couleur_vert_clair
+        TB_Affichage.ForeColor = HexToColor("#3a3a3a")
+        TB_Log.ForeColor = HexToColor("#737373")
+        L_Casio.ForeColor = couleur_vert_fonce
+        PB_Bordure_Affichage.BackColor = couleur_vert_fonce
+
+        Dim taille_bordure As Integer = 6
+
+        ' Visuel de l'écran '
+        Dim textbox = New List(Of TextBox) From {TB_Affichage, TB_Log}
+        For i = 0 To textbox.Count - 1
+            textbox(i).BackColor = HexToColor("#c0c0c0")
+            textbox(i).BorderStyle = BorderStyle.None
+        Next
+
+        ' Visuel des chiffres '
+        Dim button_chiffre = New List(Of Button) From {BT_0, BT_1, BT_2, BT_3, BT_4, BT_5, BT_6, BT_7, BT_8, BT_9}
+        For i = 0 To button_chiffre.Count - 1
+            button_chiffre(i).FlatStyle = FlatStyle.Flat
+            button_chiffre(i).FlatAppearance.BorderColor = couleur_vert
+            button_chiffre(i).FlatAppearance.BorderSize = taille_bordure
+            button_chiffre(i).BackColor = couleur_vert_fonce
+            button_chiffre(i).ForeColor = couleur_blanc
+        Next
+
+        ' Visuel des opérateurs '
+        Dim button_operation = New List(Of Button) From {BT_Ajouter, BT_Soustraire, BT_Multiplier, BT_Diviser}
+        For i = 0 To button_operation.Count - 1
+            button_operation(i).FlatStyle = FlatStyle.Flat
+            button_operation(i).FlatAppearance.BorderColor = couleur_vert_fonce
+            button_operation(i).FlatAppearance.BorderSize = taille_bordure
+            button_operation(i).BackColor = couleur_vert
+            button_operation(i).ForeColor = couleur_blanc
+        Next
+
+        ' Visuel de CE '
+        Dim button_annulation = New List(Of Button) From {BT_Changer_Signe, BT_Virgule}
+        For i = 0 To button_annulation.Count - 1
+            button_annulation(i).FlatStyle = FlatStyle.Flat
+            button_annulation(i).FlatAppearance.BorderColor = couleur_vert_fonce
+            button_annulation(i).FlatAppearance.BorderSize = taille_bordure
+            button_annulation(i).BackColor = couleur_vert_clair
+            button_annulation(i).ForeColor = couleur_vert_fonce
+        Next
+
+        ' Visuel de Supprimer '
+        BT_Supprimer.FlatStyle = FlatStyle.Flat
+        BT_Supprimer.FlatAppearance.BorderColor = HexToColor("#989231")
+        BT_Supprimer.FlatAppearance.BorderSize = taille_bordure
+        BT_Supprimer.BackColor = HexToColor("#d9d146")
+        BT_Supprimer.ForeColor = couleur_noir
+
+        ' Visuel de CE '
+        BT_CE.FlatStyle = FlatStyle.Flat
+        BT_CE.FlatAppearance.BorderColor = HexToColor("#945522")
+        BT_CE.FlatAppearance.BorderSize = taille_bordure
+        BT_CE.BackColor = HexToColor("#d47930")
+        BT_CE.ForeColor = couleur_noir
+
+        ' Visuel de C '
+        BT_C.FlatStyle = FlatStyle.Flat
+        BT_C.FlatAppearance.BorderColor = HexToColor("#82281f")
+        BT_C.FlatAppearance.BorderSize = taille_bordure
+        BT_C.BackColor = HexToColor("#b9392c")
+        BT_C.ForeColor = couleur_noir
+
+        ' Visuel de Quitter '
+        BT_Quitter.FlatStyle = FlatStyle.Flat
+        BT_Quitter.FlatAppearance.BorderColor = couleur_vert
+        BT_Quitter.FlatAppearance.BorderSize = taille_bordure
+        BT_Quitter.BackColor = couleur_vert_fonce
+        BT_Quitter.ForeColor = couleur_vert
+
+        ' Visuel de = '
+        BT_Egal.FlatStyle = FlatStyle.Flat
+        BT_Egal.FlatAppearance.BorderColor = HexToColor("#3e3980")
+        BT_Egal.FlatAppearance.BorderSize = taille_bordure
+        BT_Egal.BackColor = HexToColor("#5851b7")
+        BT_Egal.ForeColor = couleur_blanc
+
+    End Sub
 End Class
